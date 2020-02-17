484592e0f9b5d2f7f63655ab8470c89130fd7d95
Dawid <zyndadawid@gmail.com>
2020-02-17 10:53:28 +0100

    Updates

---
+++ precache-manifest.5530e0b9e1129a97bf460966699fafaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72631523cec041634365212838755a1",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "863848729572446cc205",
+    "url": "/monster/static/css/main.4c6a2d72.chunk.css"
+  },
+  {
+    "revision": "4beba386462ee8d71d1c",
+    "url": "/monster/static/js/2.a099693a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster/static/js/2.a099693a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "863848729572446cc205",
+    "url": "/monster/static/js/main.f1a56252.chunk.js"
+  },
+  {
+    "revision": "489b246603fc370cbb70",
+    "url": "/monster/static/js/runtime-main.b26981e3.js"
+  }
+]);
